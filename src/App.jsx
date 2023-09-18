361e317bb8acc1532c98c1daa90c3c8323af39fd
Karla Vujevic <karla_vu@icloud.com>
2023-09-18 18:07:05 +0200

    eventi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import { Component } from 'react'
-import {UserClass, UserChildren, UserFunction} from './Users'
+import './App.css';
+import { Component } from 'react';
+import {UserClass, UserChildren, UserFunction} from './Users';
 
 export default function App() {
   const initialUsers = [
@@ -9,20 +9,41 @@ export default function App() {
     { name: 'Sani', age: 32 },
   ];
 
-  const tekst = "Povećaj godine";
+  const tekst = "Uvećaj godine";
 
   const [users, setUsers] = useState(initialUsers);
+  const [newName, setNewName] = useState(""); 
+  const [selectedUserIndex, setSelectedUserIndex] = useState(0);
 
   const uvecajGodine = () => {
-    const newUsers = users.map(user => {
-      return { ...user, age: user.age + 1 };
-    });
-    setUsers(newUsers);
-  };
+    setUsers(prevState => {
+      const newUsers = prevState.map (user => {
+        return {...user, years: user.years + 1 }
+    })
+    return newUsers
+    })
+}
+
+const promijeniIme = () => {
+  setUsers((prevState) =>
+    prevState.map((user, index) => {
+      if (index === selectedUserIndex) {
+        return { ...user, name: newName };
+      }
+      return user;
+    })
+  );
+};
 
   return (
     <>
       <h1>State</h1>
+      <input 
+        type="text"
+        value={newName}
+        onChange={(Event) => setNewName(Event.target.value)}
+       />
+       <button onClick={promijeniIme}>Upiši novo ime</button>
       <UserClass name={users[0].name} age={users[0].age} />
       <UserFunction name={users[1].name} age={users[1].age} />
       <UserChildren name={users[2].name} age={users[2].age}>
